1ec79d08bae24135bbeeea2fdce7fd37de199238
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-07-13 13:19:51 +1000

    Create au-tld-check-synergy.php

---
+++ au-tld-check-synergy.php
@@ -0,0 +1,45 @@
+<?php
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
+
+// command line support
+if (isset($argv[1])) {
+    $_GET["domain"] = $argv[1];
+}
+
+$_GET           = filter_var_array($_GET, ["domain" => FILTER_SANITIZE_STRING]);
+$full_domain    = strtolower(trim($_GET["domain"] ?? "auda.org.au"));
+$url            = "https://manage.synergywholesale.com/whmcs_availability_checker.php";
+$timeout        = 12.0;
+$data           = http_build_query(["domain" => $full_domain]);
+
+$options = [
+    "http" => [
+        "method" => "GET",
+        "protocol_version" => 1.1,
+        "timeout" => $timeout,
+        "user_agent" => "tld-checker/1.0",
+        "header" => "Accept-language: en\r\n" .
+            "Accept: text/plain\r\n"
+        ]
+];
+
+$context        = stream_context_create($options);
+$response       = file_get_contents($url . "?" . $data);
+if ($response === false) {
+    echo "Unknown Error";
+    return;
+}
+echo $response;
+
+if (!!stristr($response, "Unavailable")) {
+    echo "Registered";
+    return;
+} else if (!!stristr($response, "Access Denied")) {
+    echo $response;
+    return;
+} else if (!!stristr($response, "Available")) {
+    echo "Not Found";
+    return;
+}
+echo "Unknown Error";
