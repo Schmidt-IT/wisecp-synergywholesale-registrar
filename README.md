3b2beea936e5cc07f3eccd6458e2119ecc6b1745
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-09-05 20:12:19 +1000

    add build script

--- README.md
+++ README.md
@@ -33,8 +33,6 @@ API Features supported:
 
 https://dev.wisecp.com/en/kb/__construct
 
-https://dev.wisecp.com/en/kb/create-a-query
-
 https://dev.wisecp.com/en/kb/cctld-gtld-whois-server
 
 https://synergywholesale.com/faq/article/api-whmcs-modules/
---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+zip -r wisecp-synergywholesale-registrar-module.zip . \
+    -x .git/\* \
+    -x .vscode/\* \
+    -x screenshots/\* \
+    -x license-error.html \
+    -x logo.jpg \
+    -x .gitignore \
+    -x .gitattributes \
+    -x build.sh
